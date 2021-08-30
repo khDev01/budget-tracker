adb1bb0a0890c80157e243b2e82e31739857b919
khDev <hussaink01@outlook.com>
2021-08-30 17:17:35 +0500

    Added new: crypt balance calculation for high0.26 low0.16 modified: Able to input non-integer amounts Added type of transaction e.g. food

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
-// Show localstorage values in console |
+// var form = document.getElementById("myForm")
+// function handleForm(event) {
+//   event.preventDefault()
+// }
+// form.addEventListener("submit", handleForm)
+
+// // Show localstorage values in console |
 for (var i = 0; i < localStorage.length; i++) {
   console.log(localStorage.getItem(localStorage.key(i)))
 }
@@ -12,27 +18,49 @@ function toggleIncomeExpense() {
   const target = document.getElementById("incomeExpense")
   target.value = target.value == "Income" ? "Expense" : "Income"
   //   target.innerHTML = target.value == "Income" ? "Expense" : "Income"
-  console.log(target)
   //   document.getElementById("count").value = 500 * value
   //   document.getElementById("totalValue").innerHTML =
   //     "Total price: $" + 500 * value
 }
+
+function toggleType() {
+  const target = document.getElementById("type")
+  target.value = target.value == "Food" ? " Bill" : "Food"
+}
+
+function toggleBuySell() {
+  const target = document.getElementById("buySell")
+  target.value = target.value == "Buy" ? " Sell" : "Buy"
+}
+
 var arr = new Array()
+var arrc = new Array()
 
 function showData() {
   getData()
   var tbl = document.getElementById("productTable")
+  var tblContent = document.getElementById("tableBody")
+
+  //  needed as page refresh is turned off for the form
+  // var x = tbl.rows.length
+  // while (--x) {
+  //   tbl.deleteRow(x)
+  //   tbl.tbl
+  // }
+  var new_tbody = document.createElement("tbody")
+  new_tbody.id = "tableBody"
+  // populate_with_new_rows(new_tbody)
 
   if (arr == null) {
     return false
   }
   for (i = 0; i < arr.length; i++) {
-    var r = tbl.insertRow()
+    var r = new_tbody.insertRow()
     var cell1 = r.insertCell()
     var cell2 = r.insertCell()
     var cell3 = r.insertCell()
-    // var cell4 = r.insertCell()
-    // var cell5 = r.insertCell()
+    // cell2.classList.add("center-align")
+    // cell3.classList.add("center-align")
     if (arr[i].income == "in" && arr[i].price > 0) {
       //   console.log(r.innerHTML)
       //   var element = document.getElementById("myDIV")
@@ -48,6 +76,39 @@ function showData() {
     // cell5.innerHTML = arr[i].income
     cell3.innerHTML = arr[i].date
   }
+  tblContent.parentNode.replaceChild(new_tbody, tblContent)
+}
+
+function showDataCrypto() {
+  getDataCrypto()
+  var tbl = document.getElementById("cryptTable")
+  var tblContentCrypto = document.getElementById("cryptTableBody")
+
+  var new_tbody_crpto = document.createElement("tbody")
+  new_tbody_crpto.id = "cryptTableBody"
+
+  if (arrc == null) {
+    return false
+  }
+  for (i = 0; i < arrc.length; i++) {
+    var r = new_tbody_crpto.insertRow()
+    var cell1 = r.insertCell()
+    var cell2 = r.insertCell()
+    var cell3 = r.insertCell()
+    var cell4 = r.insertCell()
+    var cell5 = r.insertCell()
+    cell1.innerHTML = arrc[i].type
+    cell2.innerHTML = arrc[i].buy
+    cell3.innerHTML = arrc[i].amount
+    cell4.innerHTML = arrc[i].price
+    cell5.innerHTML = arrc[i].date
+  }
+  tblContentCrypto.parentNode.replaceChild(new_tbody_crpto, tblContentCrypto)
+}
+
+function formClear() {
+  $("#productname").val("")
+  $("#price").val("")
 }
 
 function addToLocalStorage() {
@@ -62,7 +123,9 @@ function addToLocalStorage() {
   // type:$("#type").value
   incomeValue =
     document.getElementById("incomeExpense").value == "Income" ? "in" : "out"
+  type = document.getElementById("type").value == "Food" ? "food" : "bill"
   arr.push({
+    type: type,
     productname: document.getElementById("productname").value,
     price: document.getElementById("price").value,
     income: incomeValue,
@@ -71,6 +134,28 @@ function addToLocalStorage() {
 
   localStorage.setItem("localData", JSON.stringify(arr))
   $("#productname").focus()
+
+  updateTotals()
+}
+
+function addToLocalStorageCrypt() {
+  getDataCrypto()
+  type = document.getElementById("crypto").value
+  buy = document.getElementById("buySell").value
+  amount = document.getElementById("amount").value
+  price = document.getElementById("priceCrypto").value
+  fee = ""
+  rate = ""
+  arrc.push({
+    type: type,
+    buy: buy,
+    price: price,
+    amount: amount,
+    date: currentDate,
+  })
+  // console.log(localStorage.getItem("crpto"))
+
+  localStorage.setItem("crypto", JSON.stringify(arrc))
 }
 
 function getData() {
@@ -79,6 +164,15 @@ function getData() {
     return (arr = JSON.parse(str))
   }
   return (arr = new Array())
+}
+
+function getDataCrypto() {
+  var str = localStorage.getItem("crypto")
+  // console.log(str)
+  if (str != null) {
+    return (arrc = JSON.parse(str))
+  }
+  return (arrc = new Array())
 }
 
 function deleteLocalStorage() {
@@ -90,19 +184,60 @@ function deleteLocalStorage() {
 //   return false
 // }
 
-var str = localStorage.getItem("localData")
-arr = JSON.parse(str)
-
-// console.log(arr[0].price)
-if (arr != null) {
-  let total = 0
-  arr.forEach((element) => (total = total + parseInt(element.price)))
-  console.log(total)
-  document.getElementById("totalBalance").innerHTML = total
-  mybudget = document.getElementById("budget-amount").innerHTML
-  document.getElementById("budgetLeft").innerHTMl = mybudget - total
+function updateTotals() {
+  var str = localStorage.getItem("localData")
+  arr = JSON.parse(str)
+  // console.log(arr[0].price)
+  if (arr != null) {
+    let total = 0
+    arr.forEach((element) => (total = total + parseInt(element.price)))
+    // console.log(total)
+    document.getElementById("totalBalance").innerHTML = total
+    mybudget = document.getElementById("budget-amount").value
+    document.getElementById("budgetLeft").innerText = mybudget - total
+    // console.log("Budget left " + mybudget + " - " + total)
+  }
 }
+
+function saveBudget() {
+  budget = document.getElementById("budget-amount").value
+  localStorage.setItem("budget", budget)
+  updateTotals()
+}
+
+function showBudget() {
+  var savedBudget = localStorage.getItem("budget")
+  budget = document.getElementById("budget-amount")
+  budget.value = savedBudget
+}
+
+function calculate() {
+  low = parseFloat(document.getElementById("lowerNumber").value)
+  high = parseFloat(document.getElementById("higherNumber").value)
+  multiplier = parseFloat(document.getElementById("multiplier").value)
+  // lower = low * 0.0016
+  // higher = high * 0.0026
+
+  // lower = lower + low
+  // higher = high - higher
+
+  // console.log(low)
+  // console.log(lower)
+  // console.log(higher)
+  calculation = high - high * 0.0026 - (low * 0.0016 + low)
+  // difference = higher - lower
+  // console.log(typeof calculation)
+  calculation = calculation * multiplier
+  document.getElementById("calcResult").innerHTML = calculation
+  document.getElementById("roundedDownCalc").innerHTML =
+    Math.floor(calculation * 100) / 100
+  // document.getElementById("diffcalc").innerHTML = difference
+}
+
 $(document).ready(function () {
+  updateTotals()
+  showBudget()
   showData()
+  showDataCrypto()
   $("#productname").focus()
 })
